2916702f64a4d9c69bad5f5a7d29e441d286b32f
Rahul-Dev86 <88551028+Rahul-Dev86@users.noreply.github.com>
2021-08-09 01:05:39 -0400

    Add files via upload

---
+++ TwitterStatsApp.Tests/TopEmojiManagerTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using Tweetinvi;
+using TwitterStatsApp.Interfaces;
+using TwitterStatsApp.Managers;
+using TwitterStatsApp.Sevices;
+using Xunit;
+
+namespace TwitterStatsApp.Tests
+{
+    public class TopEmojiManagerTests
+    {
+        private readonly TopEmojiManager _sut;
+        private readonly TwitterAuthService _ta;
+        private readonly Mock<IExceptionLogService> _expLogSrvMock = new Mock<IExceptionLogService>();
+        private readonly Mock<IConfiguration> _configMock = new Mock<IConfiguration>();
+        public TopEmojiManagerTests()
+        {
+            _ta = new TwitterAuthService(_configMock.Object, _expLogSrvMock.Object);
+            _sut = new TopEmojiManager(_expLogSrvMock.Object);
+        }
+
+        [Fact]
+        public async Task getTopEmoji_ShouldReturn_EmojiCount()
+        {
+            //Arrange
+            TwitterClient appClient = await _ta.GetAppClientForUnitTests();
+            string[] emojis = new string[] { "😀:Happy Face", "😍:Heart Face", "🤓:Nerdy", "💖:Sparkle Heart", "😹:Happy Tears Cat", "💯:Hundred Points Symbol", "👋🏻:Waving Hand", "🚩:Triangular Flag", "🌝:Moonface", "🎉:Celebration", "🎯:Archery", "🏆:Trophy" };
+            string endpoint = "https://api.twitter.com/2/tweets/counts/recent?query=";
+            //Act
+            KeyValuePair<string, Int64> topEmoji = await _sut.getTopEmojiAsync(appClient, endpoint, emojis);
+            //Assert 
+            Assert.True(topEmoji.Value > 0);
+            //Assert.NotEmpty(topEmoji.Key);
+        }
+    }
+}
---
+++ TwitterStatsApp.Tests/TweetCountManagerTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using Tweetinvi;
+using TwitterStatsApp.Interfaces;
+using TwitterStatsApp.Managers;
+using TwitterStatsApp.Models;
+using TwitterStatsApp.Sevices;
+using Xunit;
+
+namespace TwitterStatsApp.Tests
+{
+    public class TweetCountManagerTests
+    {
+        private readonly TweetCountManager _sut;
+        private readonly TwitterAuthService _ta;
+        private readonly Mock<IExceptionLogService> _expLogSrvMock = new Mock<IExceptionLogService>();
+        private readonly Mock<IConfiguration> _configMock = new Mock<IConfiguration>();
+        public TweetCountManagerTests()
+        {
+            _sut = new TweetCountManager(_expLogSrvMock.Object);
+            _ta = new TwitterAuthService(_configMock.Object, _expLogSrvMock.Object);
+        }
+
+        [Fact]
+        public async Task getTweetCount_ShouldReturn_TweetCount()
+        {
+            //Arrange
+            TwitterClient appClient = await _ta.GetAppClientForUnitTests();
+            string query = "covid&granularity=day&start_time=" + Uri.EscapeDataString(DateTime.Now.AddDays(-1).ToString("yyyy-MM-ddTHH\\:mm\\:ss.fffffffzzz"));
+            string endpoint = "https://api.twitter.com/2/tweets/counts/recent?query="+query;
+            //Act
+            List<TweetCountVM> objTweetCount = await _sut.getTweetCountAsync(appClient, endpoint);
+            //Assert
+            Assert.NotNull(objTweetCount);
+        }
+    }
+}
